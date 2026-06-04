92558fc70843aa9b024d6939d73d678b10313336
KandyKaneMarvel <kpollock729@gmail.com>
2026-06-04 10:34:42 -0500

    stuff -kane

--- Telephone.java
+++ Telephone.java
@@ -1,10 +1,12 @@
 public class Telephone {
     public static void main(String[] arg) {
-        int num = 5;
+        int num = 3;
         num = subNum(num);
         num = modNum(num);
         num = (int)squareRoot(num);
-        
+        num = expNum(num);
+
+
     }
 
     public static int subNum(int y) {
@@ -16,7 +18,10 @@ public class Telephone {
     }
 
     public static double squareRoot(int s) {
-        return (int)Math.sqrt(s);
+        return (int) Math.sqrt(s);
     }
     
+    public static int expNum(int k) {
+        return (int) Math.pow(k, 4);
+    }
 }
