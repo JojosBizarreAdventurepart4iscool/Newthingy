698ebd08573434197f8e15d92cad781335586829
plasticdinos609 <bidigubkii@gmail.com>
2026-06-04 10:05:17 -0500

    upd8

--- Telephone.java
+++ Telephone.java
@@ -1,9 +1,15 @@
 public class Telephone {
     public static void main(String[] arg) {
         int num = 5;
-
+        num = subNum(num);
+        num = modNum(num);
     }
-    public static int subNum(int y){
+
+    public static int subNum(int y) {
         return 20 - y;
+    }
+
+    public static int modNum(int z) {
+        return z % 10;
     }
 }
