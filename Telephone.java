c0eb57d4221a7ae7c7f3d162fb4f9044f29f9461
The-Esc-key <fieldsbran05@gmail.com>
2026-06-04 10:18:40 -0500

    Brandan

--- Telephone.java
+++ Telephone.java
@@ -3,6 +3,7 @@ public class Telephone {
         int num = 5;
         num = subNum(num);
         num = modNum(num);
+        num = (int)squareRoot(num);
     }
 
     public static int subNum(int y) {
@@ -11,5 +12,9 @@ public class Telephone {
 
     public static int modNum(int z) {
         return z % 10;
+    }
+
+    public static double squareRoot(int s) {
+        return (int)Math.sqrt(s);
     }
 }
